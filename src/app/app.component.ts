da996fefc87cba13774b58164a9a7606da728ff8
sholom <sholomls@gmail.com>
2019-05-02 03:07:40 -0400

    Event Emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ogbonna Vitalis, Welcome to Angular';
+  title = 'my-app';
+  name = 'Sholom';
+  public message = '';
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-//import {window} from 'rxjs/operators';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+// import {window} from 'rxjs/operators';
 
 @Component({
   selector: 'app-test',
@@ -7,36 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-public name = '';
-public myId = 'testId';
-public isDisabled = true;
-public successClass = 'text-success'
-public siteUrl = window.location.href;
-public hasError = false;
-public isSpecial = true;
-public greeting = '';
-public messageClasses = {
-  'text-success': !this.hasError,
-  'text-danger': this.hasError,
-  'text-special': this.isSpecial
-}
-
-public titleStyles = {
-  color: 'blue',
-  fontStyle: 'italic'
-}
-
-public displayName = true;
+@Input('parentData') public name;
+@Output() public childEvent = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
   }
 
   greetUser(){
-    return 'Welcome ' + this.name;
+   // return 'Welcome ' + this.name;
   }
 logMessage(e) {
 
   console.log(e);
+}
+
+fireEvent(){
+this.childEvent.emit('Hey Codevolution');
 }
 }
